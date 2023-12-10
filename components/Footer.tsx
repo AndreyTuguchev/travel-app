45c5d9c95abab18e9c3c732a5d3c354c2d9f051a
Andrey <tuguchev.andrey@gmail.com>
2023-12-11 03:12:39 +0600

    fix: typescript errors and upd next.config.js

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,13 +12,16 @@ const Footer = () => {
             entries.forEach(function(entry) {
                 if (entry.isIntersecting) {
                     let lazyImage = entry.target;
-                    console.log("lazyImage.dataset.src ", lazyImage.dataset.src)
-                    if (lazyImage.dataset){
-                        if ( lazyImage.dataset.src.indexOf("data:image") === -1 && lazyImage.src.indexOf("data:image") !== -1 ){
-                            lazyImage.src = lazyImage.dataset.src
-                            lazyImage.classList.remove("lazyload");
-                            lazyImage.classList.add("lazyloaded");
-                            imageObserver.unobserve(lazyImage)
+                    if ( lazyImage instanceof HTMLImageElement){
+
+                        console.log("lazyImage.dataset.src ", lazyImage.dataset.src)
+                        if (lazyImage.dataset){
+                            if ( lazyImage.dataset.src !== undefined && lazyImage.dataset.src.indexOf("data:image") === -1 && lazyImage?.src.indexOf("data:image") !== -1 ){
+                                lazyImage.src = lazyImage.dataset.src
+                                lazyImage.classList.remove("lazyload");
+                                lazyImage.classList.add("lazyloaded");
+                                imageObserver.unobserve(lazyImage)
+                            }
                         }
                     }
                 }
--- next.config.js
+++ next.config.js
@@ -3,25 +3,13 @@
 
 const nextConfig = {
     experimental: { optimizeCss: true },
+    
     logging: {
         fetches: {
           fullUrl: true,
         },
       },
-    // webpack: (config, options) => {
-    //     config.module.rules.push({
-    //         test: /\.css$/i,
-    //         use: [
-    //         {
-    //             loader: "style-loader",
-    //             options: { injectType: "singletonStyleTag" },
-    //         },
-    //         "css-loader",
-    //         ],
-    //     })
-     
-    //     return config
-    //   },
+    
 
 }
 
