e3c08cf7f9b43e5a883d3608f2d821ad9187e0aa
Andrey <tuguchev.andrey@gmail.com>
2023-12-11 03:14:17 +0600

    fix: logging deactivated

--- next.config.js
+++ next.config.js
@@ -3,14 +3,7 @@
 
 const nextConfig = {
     experimental: { optimizeCss: true },
-    
-    logging: {
-        fetches: {
-          fullUrl: true,
-        },
-      },
-    
-
+   
 }
 
 module.exports = nextConfig
