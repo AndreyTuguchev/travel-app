0c5db1fa24fcd1ce62136da986ca4936efcf3957
Andrey <tuguchev.andrey@gmail.com>
2023-12-11 02:35:49 +0600

    feat: section lazyload-bg class added to lz bg IMGs

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Guide = () => {
     return (
-        <section className="flexCenter flex-col">
+        <section className="lazyload-bg flexCenter flex-col">
             <div className="padding-container max-container w-full pb-24">
                 <Image
                     src='data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
